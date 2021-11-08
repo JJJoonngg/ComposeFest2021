a9bbef112b17eed4ac126b29906720520aa470d8
JJJoonngg <whdtls3878@gmail.com>
2021-11-08 21:57:39 +0900

    5. Reusing composables

--- week 1-Jetpack Compose basics/Basic-Codelab-compose/app/src/main/java/kr/co/jjjoonngg/basic_codelab_compose/MainActivity.kt
+++ week 1-Jetpack Compose basics/Basic-Codelab-compose/app/src/main/java/kr/co/jjjoonngg/basic_codelab_compose/MainActivity.kt
@@ -18,12 +18,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicCodelabcomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
+    }
+}
+
+@Composable
+private fun MyApp() {
+    Surface(color = MaterialTheme.colors.background) {
+        Greeting("Android")
     }
 }
 
@@ -38,6 +42,6 @@ private fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     BasicCodelabcomposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
